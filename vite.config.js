8d3d52a75dfa0a8d63b70077659839e67fe65c92
KyanPepper <kyankks1@gmail.com>
2024-01-21 23:18:10 -0800

    tailwind setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import postcss from './postcss.config.js';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  css:{
+    postcss
+  }
 })
