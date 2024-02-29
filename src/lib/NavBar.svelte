653733e194ec59ac6101c695d8694b479de0d76f
KyanPepper <kyankks1@gmail.com>
2024-02-29 00:28:51 -0800

    nav fixed css

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="w-full h-16 sm:h-20 bg-stone-600 bg-opacity-5">
   <div class="flex items-center justify-between h-full px-4 sm:px-8">
-    <div class="text-stone-300 text-3xl font-sans flex items-center ">
+    <div class="text-stone-300 text-3xl font-sans flex items-center pl-5">
       Kyan   
       <img
         src="src\lib\imgs\orange-4235372_1280.png"
@@ -12,14 +12,13 @@
         class="h-8 sm:h-12 ml-4"
       />
     </div>
-
-    <div class="flex items-center space-x-4">
-      <a href="/" class="text-stone-100 text-2xl">Expierence</a>
-      <a href="/about" class="text-stone-300 font-sans text-2xl">About</a>
-      <a href="/blog" class="text-stone-300 font-sans text-2xl">Blog</a>
+    <div class="flex items-center space-x-4 justify-center">
+      <a href="/" class="text-stone-300 text-2xl">Experience</a>
+      <a href="/about" class="text-stone-300 font-sans text-2xl">Resume</a>
+      <a href="/blog" class="text-stone-300 font-sans text-2xl">Projects</a>
     </div>
-    <div class="flex items-center space-x-4">
-      <a href="/login" class="text-stone-300 font-sans text-2xl">Contact</a>
+    <div class=" items-center justify-center pr-5">
+      <a href="/login" class="text-stone-300 font-sans text-3xl">Contact</a>
     </div>
   </div>
 </nav>
