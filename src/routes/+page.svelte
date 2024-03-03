025ea1387b63df01ee88c78d3b0a0ce60b01e7e6
KyanPepper <kyankks1@gmail.com>
2024-03-02 22:42:32 -0800

    refractored main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
-
 <div class="flex">
-  
-    <div class="w-1/2">
-      <p>
-        text here
-      </p>
-    </div>
-  
-
-    <div class="flex justify-center items-center h-screen ">
-          <div class="flex justify-center items-center w-1/2 h-1/2">
-            <img src="src\lib\imgs\kyansnow.jpg" alt="kyansnow" class="h-auto w-auto outline outline-white" >
-        </div>
+    <div class="w-1/2 flex justify-center items-center h-screen">
+      <div class="text-center">
+        <h1 class="text-4xl text-white">
+          Hi, I'm Kyan Kotschevar-Smead, a software developer and a computer science student at Washington State University
+        </h1>
       </div>
-      
+    </div>
+    <div class="w-1/2 flex justify-center items-center h-screen">
+      <div class="w-4/5 h-4/5">
+        <img
+          src="src\lib\imgs\kyansnow.jpg"
+          alt="kyansnow"
+          class="h-auto w-auto outline outline-white"
+        />
+      </div>
+    </div>
   </div>
   
