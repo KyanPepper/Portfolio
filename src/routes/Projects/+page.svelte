72fe3bc0b64e16f8bb587bdc18ba6f2a52685d73
KyanPepper <kyankks1@gmail.com>
2024-03-09 23:30:51 -0800

    slangspeak + next and prev button functionality

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -1,11 +1,33 @@
 <script>
+    import { onMount, createEventDispatcher } from "svelte";
     import ProjectCard from "$lib/Assets/ProjectCard.svelte";
     import { ProjectCards } from "./ProjectData";
+
+    const dispatch = createEventDispatcher();
+    let currentIndex = 0;
+
+    function goToPrevious() {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = ProjectCards.length - 1;
+        }
+        dispatch("updateIndex", currentIndex);
+    }
+
+    function goToNext() {
+        if (currentIndex < ProjectCards.length - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        dispatch("updateIndex", currentIndex);
+    }
 </script>
 
 <div id="controls-carousel" class="flex justify-between items-center w-full h-max" data-carousel="static">
     <!-- Previous Button -->
-    <button type="button" class="relative top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+    <button on:click={goToPrevious} type="button" class="relative top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -16,13 +38,11 @@
 
     <!-- Project Card -->
     <div class="relative flex justify-center items-center h-full w-8/12 py-5 drop-shadow-2xl">
-        {#each ProjectCards as Card}
-            <ProjectCard {...Card} />
-        {/each}
+        <ProjectCard {...ProjectCards[currentIndex]} />
     </div>
 
     <!-- Next Button -->
-    <button type="button" class="realtive top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+    <button on:click={goToNext} type="button" class="realtive top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
@@ -31,3 +51,4 @@
         </span>
     </button>
 </div>
+
--- src/routes/Projects/ProjectData.js
+++ src/routes/Projects/ProjectData.js
@@ -23,5 +23,27 @@ export const ProjectCards = [
     YtUrl: "https://www.youtube.com/watch?v=x_e42T3naCo&ab_channel=KyanKotschevar-Smead",
     DeployedUrl: "http://www.washconstruction.com/",
   },
+  {
+    //SlangSpeak
+    TechImgs: [
+      "src/lib/imgs/1200px-Python.svg.png",
+      "src/lib/imgs/django-icon.svg",
+      "src/lib/imgs/sqlLite.png",
+      "src/lib/imgs/React.png",
+      "src/lib/imgs/JavaScript-logo.png"
+    ],
 
+    Title: "SlangSpeak",
+    About:
+      "SlangSpeak is a language learning platform, similar to Duolingo, but focused on teaching slang words. You can practice or take exams, signing in through SlangSpeak to track progress. Exams consist of random slang terms and their definitions, while Practice Mode uses the same bank without affecting scores.",
+    img1: "src/lib/ProjectScreenShots/slangspeak2.png",
+    img2: "src/lib/ProjectScreenShots/slangspeak1.png",
+    Frontend:
+      "The Frontend was built with React class based components utilizing MUI for the UI.",
+    Backend:
+      "The backend was made with Django and SQLite and uses Django’s authentication, groups and token generation. With these tools I created various API endpoints for user interactions, including signup, login, and score tracking.",
+    GhUrl: "https://github.com/KyanPepper/slang-speak",
+    YtUrl: "https://www.youtube.com/watch?v=T07W9B8Sh3o&ab_channel=KyanKotschevar-Smead",
+    DeployedUrl: "",
+  },
 ];
