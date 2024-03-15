7dccb99efd2020840d1d5524d1314e2977754b69
KyanPepper <kyankks1@gmail.com>
2024-03-14 23:25:21 -0700

    img test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+  import githubLogo from 'src/lib/imgs/githublogo.png';
+  import gmailIcon from 'src/lib/imgs/Gmail_icon_(2020).svg.webp';
+  import linkedinLogo from 'src/lib/imgs/linkedin.png';
+  import youtubeLogo from 'src/lib/imgs/Red-YouTube-logo.png';
+  import kyansnowImage from 'src/lib/imgs/kyansnow.jpg';
+</script>
+
 <div class="tablet:flex">
   <!-- Desktop design -->
   <div class="hidden tablet:flex justify-center items-center h-screen w-1/2">
@@ -8,39 +16,33 @@
       </h1>
       <div class="pt-5">
         <a href="/Projects">
-        <button
-          class="bg-orange-500 hover:bg-orange-400 text-white font-sans font-bold py-2 px-4 rounded"
-        >
-          Let's Get Started
-        </button>
-      </a>
+          <button
+            class="bg-orange-500 hover:bg-orange-400 text-white font-sans font-bold py-2 px-4 rounded"
+          >
+            Let's Get Started
+          </button>
+        </a>
 
         <div class="mt-4 text-white font-sans text-xs">
           <div class="flex justify-center">
             <div class="flex">
               <a href="https://github.com/KyanPepper" class="flex items-center">
-                <img src="src/lib/imgs/githublogo.png" class="size-5" />
+                <img src={githubLogo} class="size-5" />
                 <span class="pl-1 pr-5">KyanPepper</span>
               </a>
               <a href="mailto:kyankks1@gmail.com" class="flex items-center">
-                <img
-                  src="src\lib\imgs\Gmail_icon_(2020).svg.webp"
-                  class="size-4"
-                />
+                <img src={gmailIcon} class="size-4" />
                 <span class="pl-1 pr-5">Kyankks1</span>
               </a>
               <a
                 href="https://www.linkedin.com/in/kyan-kotschevar-smead-560862204/"
                 class="flex items-center"
               >
-                <img src="src\lib\imgs\linkedin.png" class="size-5" />
+                <img src={linkedinLogo} class="size-5" />
                 <span class="pl-1 pr-4">Kyan Kotschevar-Smead</span>
               </a>
-              <a
-                href="https://www.youtube.com/@kyanks"
-                class="flex items-center"
-              >
-                <img src="src/lib/imgs/Red-YouTube-logo.png" class="w-7 h-4" />
+              <a href="https://www.youtube.com/@kyanks" class="flex items-center">
+                <img src={youtubeLogo} class="w-7 h-4" />
                 <span class="">Kyanks</span>
               </a>
             </div>
@@ -53,7 +55,7 @@
   <div class="hidden w-1/2 tablet:flex justify-center items-center h-screen">
     <div class="w-4/5 h-4/5">
       <img
-        src="src\lib\imgs\kyansnow.jpg"
+        src={kyansnowImage}
         alt="kyansnow"
         class="h-auto w-auto outline outline-white"
       />
@@ -64,7 +66,7 @@
   <div class="tablet:hidden w-11/12 pt-10 ml-auto mr-auto z-30">
     <div class="justify-center">
       <img
-        src="src\lib\imgs\kyansnow.jpg"
+        src={kyansnowImage}
         alt="kyansnow"
         class="h-auto w-auto outline outline-white"
       />
@@ -83,25 +85,25 @@
   <div
     class="tablet:hidden ml-auto mr-auto justify-center pt-5 pb-10 flex flex-col items-center"
   >
-  <a href="/Projects">
-    <button
-      class="bg-orange-500 hover:bg-orange-400 text-white font-sans font-bold py-2 px-4 rounded"
-    >
-      Let's Get Started
-    </button>
-  </a>
+    <a href="/Projects">
+      <button
+        class="bg-orange-500 hover:bg-orange-400 text-white font-sans font-bold py-2 px-4 rounded"
+      >
+        Let's Get Started
+      </button>
+    </a>
     <div
       class="mt-4 text-white font-sans text-xs flex flex-wrap justify-center"
     >
       <div class="flex items-center mr-4 mb-2">
         <a href="https://github.com/KyanPepper" class="flex items-center">
-          <img src="src/lib/imgs/githublogo.png" class="size-5" />
+          <img src={githubLogo} class="size-5" />
           <span class="pl-1">KyanPepper</span>
         </a>
       </div>
       <div class="flex items-center mr-4 mb-2">
         <a href="mailto:kyankks1@gmail.com" class="flex items-center">
-          <img src="src\lib\imgs\Gmail_icon_(2020).svg.webp" class="size-4" />
+          <img src={gmailIcon} class="size-4" />
           <span class="pl-1">Kyankks1</span>
         </a>
       </div>
@@ -110,13 +112,13 @@
           href="https://www.linkedin.com/in/kyan-kotschevar-smead-560862204/"
           class="flex items-center"
         >
-          <img src="src\lib\imgs\linkedin.png" class="size-5" />
+          <img src={linkedinLogo} class="size-5" />
           <span class="pl-1">Kyan Kotschevar-Smead</span>
         </a>
       </div>
       <div class="flex items-center mr-4 mb-2">
         <a href="https://www.youtube.com/@kyanks" class="flex items-center">
-          <img src="src/lib/imgs/Red-YouTube-logo.png" class="w-7 h-4" />
+          <img src={youtubeLogo} class="w-7 h-4" />
           <span class="">Kyanks</span>
         </a>
       </div>
